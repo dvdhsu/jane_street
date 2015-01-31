9937e96a202516c08070ea9be382a95453c389ce
Anthony Guo <anthony.guo@some.ox.ac.uk>
2015-01-31 11:35:24 +0000

    Added buying method

--- model.js
+++ model.js
@@ -1,20 +1,44 @@
+var order_id = 0;
+var order_id_to_order = {};
+
+function getNewOrderId(){
+    return order_id;
+    order_id += 1;
+}
+
+
+global.socket;
 global.cash;
 global.market_opened;
 global.symbols = {};
 
 global.logPosition= function(){
-  console.log('cash: ' + global.cash);
-  console.log('market opened: ' + global.market_opened);
-  for (var symbol in global.symbols){
-    console.log('Symbol: ' + symbol + ', position: ' + global.symbols[symbol]);
-  }
+    console.log('cash: ' + global.cash);
+    console.log('market opened: ' + global.market_opened);
+    for (var symbol in global.symbols){
+        console.log('Symbol: ' + symbol + ', position: ' + global.symbols[symbol]);
+    }
 }
 
-
-order_ids = 0;
-order_id_to_order = {};
+global.send = function(msg){
+  console.log('Sending: ' + msg);
+  msg += '\n';
+  global.socket.write(msg);
+}
 
 global.buyPosition = function(symbol, price, size){
-  
+    var new_order_id = getNewOrderId();
+    var orderObj = {
+        type: 'add',
+        order_id : new_order_id,
+        symbol: symbol,
+        dir: 'BUY',
+        price: price,
+        size: size
+    };
+    var msg = JSON.stringify(orderObj);
+    order_id_to_order[new_order_id] = msg;
+    global.send(msg);
+
 }
 
--- receiver.js
+++ receiver.js
@@ -9,56 +9,49 @@ var PORT = 25000;
 var TEAM_NAME = 'POMEGRANATE';
 
 console.log("PLEASE WORK");
-var client = new net.Socket();
-client.connect(PORT, HOST, function() {
+global.socket = new net.Socket();
+global.socket.connect(PORT, HOST, function() {
 
-  console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
 
-  // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
-  var helloMsg = JSON.stringify({type: 'hello', team: TEAM_NAME});
-  send_log(helloMsg);
+    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
+    var helloMsg = JSON.stringify({type: 'hello', team: TEAM_NAME});
+    global.send(helloMsg);
 
 });
 
-
-function send_log(msg){
-  console.log('Sending: ' + msg);
-  msg += '\n';
-  client.write(msg);
-}
 
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
-client.on('data', function(unparsed_data) {
-  global.logPosition();
-  var lines = unparsed_data.toString().split('\n');
-  for (var i =0; i != lines.length; ++i){
-try {
-  parsed_data = JSON.parse(lines[i]);
-}
-catch (err) {
-  continue;
-}
-  switch(parsed_data["type"]) {
-    case "hello":
-      global.cash = parsed_data.cash;
-      for (var ticker in parsed_data.symbols) {
-        global.symbols[ticker.symbol] = ticker.position;
-      }
-      global.market_opened = parsed_data.market_open;
-      break;
+global.socket.on('data', function(unparsed_data) {
+    global.buyPosition('FOO',23, 142);
+    global.logPosition();
+    var lines = unparsed_data.toString().split('\n');
+    for (var i =0; i != lines.length; ++i){
+        try {
+            parsed_data = JSON.parse(lines[i]);
+        }
+        catch (err) {
+            continue;
+        }
+        switch(parsed_data["type"]) {
+            case "hello":
+                global.cash = parsed_data.cash;
+                for (var ticker in parsed_data.symbols) {
+                    global.symbols[ticker.symbol] = ticker.position;
+                }
+                global.market_opened = parsed_data.market_open;
+                break;
 
-    case "market_open":
-      global.market_opened = parsed_data.open;
-      break;
-      // etc. with cases
-  }
-}
-  // Close the client socket completely
-  //client.destroy();
+            case "market_open":
+                global.market_opened = parsed_data.open;
+                break;
+                // etc. with cases
+        }
+    }
 });
 
 // Add a 'close' event handler for the client socket
-client.on('close', function() {
-  console.log('Connection closed');
+global.socket.on('close', function() {
+    console.log('Connection closed');
 });
